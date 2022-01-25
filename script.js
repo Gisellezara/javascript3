399d3bc5430f4a9e6fd377ddb8eff19c25f33224
Gisellezara <94918486+Gisellezara@users.noreply.github.com>
2022-01-24 23:48:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//EASY
+
+function run(){
+    return "running"
+}
+console.log("Todays exercise:" + run()); 
+
+function swim(){
+    return "swimming"
+}
+console.log("Todays exercise:" + swim()); 
+
+//Medium 
+// prints "Each person gets 4.00 slices of pizza"
+
+// var sharePizzaMat = cutPizzaSlices / peoplePerPizza
+var cutPizzaSlices= 8;
+function sharePizza(x) {
+    return "Each person gets " + 8/(x) + " slices of pizza"
+};
+console.log(sharePizza(2)); 
+console.log(sharePizza(3));
+
+
+//hard 
+const ssn= private
+var people= {
+    fullName: "Giselle Guevara"
+    ssn: 123456789},{
+    fullName: "Alec Franco"
+    ssn:987654321
+    return 
+};
+
+#privateMethod(){
+    return 'Hello world'
+}
+console.log(instanceof.privateMethod);
+ 
+
