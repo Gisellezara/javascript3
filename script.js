63d48bb05dc04cb5ced018bdf8f4020ae5abb995
Gisellezara <94918486+Gisellezara@users.noreply.github.com>
2022-01-25 00:56:50 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -22,19 +22,36 @@ console.log(sharePizza(2));
 console.log(sharePizza(3));
 
 
-//hard 
-const ssn= private
-var people= {
-    fullName: "Giselle Guevara"
-    ssn: 123456789},{
-    fullName: "Alec Franco"
-    ssn:987654321
-    return 
-};
+//hard   
+let ssn= Symbol();
+let person= (function(){
+   class person{
+       constructor(name){
+           this.name= 'giselle Guevara';
+           this[ssn]=123456789;
+       }  
+       }
+       return person;
+   } )();
+  console.log(person);
 
-#privateMethod(){
-    return 'Hello world'
+  //very hard 
+
+  class person1{
+    constructor(name, job, age){
+        this.name= name,
+        this.job= job,
+        this.age= age,
+        this.exercise= "running is not fun";
+    }
+    console.log(this.exercise);
+    sayName(){
+        console.log(`${this.name}`);
+        console.log(`${this.name} is a  back-end developer at ${this.job}`);
+        this.exercise="running is not fun";
+    }
 }
-console.log(instanceof.privateMethod);
- 
+const person1 = new person1("Giselle", "R2H", "24");
+
+Giselle.sayName();
 
